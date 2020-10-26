a13cfec1180bf52d975ca5a12e31032b83dacc5b
atenaiis <attennaisc@gmail.com>
2020-10-26 15:54:49 -0600

    got first tweets posted using gem

--- lib/work.rb
+++ lib/work.rb
@@ -1,26 +1,18 @@
-require 'twitter' #gem install twitter
-while true
-    begin
+
+require 'twitter'
+
+
         # Create a read write application from : 
         # https://apps.twitter.com
         # authenticate it for your account
         # fill in the following
-        config = {
-            consumer_key:         'tkzJDaDrUud1hgtX8rR6k3GyC',
-            consumer_secret:     'CccgeoD0gKtscY2YKWF97iZWJLfJdxEo0fgoAa3zI2OP6vkeEb',
-            access_token:        '744842330050465793-W7uiqCUx5mIH5yyQvGOLcZl2sqrEmlw',
-            access_token_secret: 'Kq6KQDoVBt3Y7jEio6VCyIiAwU21v4Purrj1dETErckIG'
-        }
-        rClient = Twitter::REST::Client.new config
-        sClient = Twitter::Streaming::Client.new(config)
-
-        # topics to watch
-        topics = ['#rails', '#rails']
-      
+        client = Twitter::REST::Client.new do |config|
+            config.consumer_key =         'tkzJDaDrUud1hgtX8rR6k3GyC'
+            config.consumer_secret =     'CccgeoD0gKtscY2YKWF97iZWJLfJdxEo0fgoAa3zI2OP6vkeEb'
+            config.access_token =        '744842330050465793-W7uiqCUx5mIH5yyQvGOLcZl2sqrEmlw'
+            config.access_token_secret = 'Kq6KQDoVBt3Y7jEio6VCyIiAwU21v4Purrj1dETErckIG'
         end
-    rescue
-        puts 'error occurred, waiting for 5 seconds'
-        sleep 5
-    end
-
-end
+        client.search('#ruby', result_type: "recent").take(3).each do |tweet|
+        client.update  ("#{tweet.user.screen_name}: #{tweet.text}")
+          end
+          
