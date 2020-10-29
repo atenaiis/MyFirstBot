183f222960f94de7b74b1c1b753ab94c87257ef7
atenaiis <attennaisc@gmail.com>
2020-10-29 16:09:10 -0600

    runing rubocop

--- lib/work.rb
+++ lib/work.rb
@@ -2,23 +2,21 @@ require 'twitter'
 require 'dotenv'
 Dotenv.load('.env_sample')
 
-while true
-    begin
-        client = Twitter::REST::Client.new do |config|
-
-      config.consumer_key = ENV['CONSUMER_KEY']
-      config.consumer_secret = ENV['CONSUMER_SECRET']
-      config.access_token = ENV['ACCESS_TOKEN']
-      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
-    end
-
-    client.search('#chocolate', result_type: "recent").take(3).each do |tweet|
-    client.update  ("#{tweet.user.screen_name}: #{tweet.text}:Hey I love chocolate too, what are your favorite blogs? ")
-  client.fav tweet    
-  end
-
- rescue
-      puts 'error occurred, waiting for 5 seconds'
-      sleep 5
+loop do
+  begin
+   client = Twitter::REST::Client.new do |config|
+     config.consumer_key = ENV['CONSUMER_KEY']
+     config.consumer_secret = ENV['CONSUMER_SECRET']
+     config.access_token = ENV['ACCESS_TOKEN']
+     config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
    end
+
+   client.search('#chocolate', result_type: 'recent').take(3).each do |tweet|
+     client.update "#{tweet.user.screen_name}: #{tweet.text}:Hey I love chocolate too, what are your favorite blogs? "
+     client.fav tweet
+   end
+ rescue StandardError
+   puts 'error occurred, waiting for 5 seconds'
+   sleep 5
  end
+end
