cf46247aafc4fe7c4f0eb210f94f6861a5325dbf
atenaiis <attennaisc@gmail.com>
2020-10-29 13:59:44 -0600

    added dotenv

--- bin/main.rb
+++ bin/main.rb
@@ -1,13 +1,5 @@
 require'twitter'
-
-while true
-begin
-        client = Twitter::REST::Client.new do |config|
-            config.consumer_key =         'tkzJDaDrUud1hgtX8rR6k3GyC'
-            config.consumer_secret =     'CccgeoD0gKtscY2YKWF97iZWJLfJdxEo0fgoAa3zI2OP6vkeEb'
-            config.access_token =        '744842330050465793-W7uiqCUx5mIH5yyQvGOLcZl2sqrEmlw'
-            config.access_token_secret = 'Kq6KQDoVBt3Y7jEio6VCyIiAwU21v4Purrj1dETErckIG'
-        end
+require 'work'
        
         client.search('#chocolate', result_type: "recent").take(3).each do |tweet|
         client.update  ("#{tweet.user.screen_name}: #{tweet.text}:Hey I love chocolate too, what are your favorite blogs? ")
